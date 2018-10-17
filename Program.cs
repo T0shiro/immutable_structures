f33f0dd7f575fb3f01bffa783215953e52afbe28
Unknown <loic.gardaire@gmail.com>
2018-10-17 10:40:26 +0200

    Mutable red black tree implementation

--- Program.cs
+++ Program.cs
@@ -7,10 +7,17 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-            BinaryTree<int> binaryTree = new BinaryTree<int>();
-            binaryTree.Add(1);
-            Console.WriteLine(binaryTree.GetHeight());
-            Console.WriteLine("Hello World!");
+            RBTree tree = new RBTree();
+            tree.Insert(5);
+            tree.Insert(3);
+            tree.Insert(7);
+            tree.Insert(1);
+            tree.Insert(9);
+            tree.Insert(11);
+            tree.Insert(6);
+            tree.Delete(9);
+            tree.Delete(5);
+            tree.DisplayTree();
             Console.ReadLine();
         }
     }
