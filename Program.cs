53aac4a06f4e3d2643ae572563f74529a2a6a847
Loic Gardaire <loic.gardaire@gmail.com>
2018-10-27 19:22:13 +0200

    correct mistake pow

--- Program.cs
+++ Program.cs
@@ -6,22 +6,22 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-            int MAX_TWO_POW = 9;
+            int MAX_TWO_POW = 10;
             int ALGO_ITERATIONS = 100;
             runAVLTree runner = new runAVLTree();
             Console.WriteLine("MAX ARRAY SIZE = 2^"+MAX_TWO_POW+", ITERATIONS FOR EACH = "+ALGO_ITERATIONS);
             Console.WriteLine("Creation of tree : ");
-            for (int i = 2; i < Math.Pow(2, MAX_TWO_POW)+1; i *= 2)
+            for (int i = 2; i <= Math.Pow(2, MAX_TWO_POW); i *= 2)
             {
                 Console.WriteLine(i+" : "+runner.RunCreation(i, ALGO_ITERATIONS));
             }
             Console.WriteLine("\nInsertion of minimum : ");
-            for (int i = 2; i < Math.Pow(2, MAX_TWO_POW)+1; i *= 2)
+            for (int i = 2; i <= Math.Pow(2, MAX_TWO_POW); i *= 2)
             {
                 Console.WriteLine(i+" : "+runner.RunInsertion(i, ALGO_ITERATIONS));
             }
             Console.WriteLine("\nDeletion of minimum : ");
-            for (int i = 2; i < Math.Pow(2, MAX_TWO_POW)+1; i *= 2)
+            for (int i = 2; i <= Math.Pow(2, MAX_TWO_POW); i *= 2)
             {
                 Console.WriteLine(i+" : "+runner.RunDeletion(i, ALGO_ITERATIONS));
             }
